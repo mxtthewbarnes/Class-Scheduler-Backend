9efe6ee4cb775e3d20642839733fff24f54ba498
Matthew Barnes <matthew.m.barnes@wsu.edu>
2025-12-04 21:34:29 -0800

    db.js fix for render.com

--- config/db.js
+++ config/db.js
@@ -1,25 +1,9 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
-const isProduction = process.env.NODE_ENV === "production";
-
-let pool;
-
-if (process.env.DATABASE_URL && isProduction) {
-  //Railway
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  });
-} else {
-  pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: 5432,
-  });
-}
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
